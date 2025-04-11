3ad84f0def61d52d054942ffbdcf18a1426d48d3
FarazAyaz <ayazf313@gmail.com>
2025-04-11 16:02:01 +0500

    condition randering

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ const App = () => {
     const rating = 9.5;
     const genre = "Action, Adventure, Drama";
     const summary = "Young Thorfinn grew up listening to the stories of old sailors that had traveled the ocean and reached the place of legend, Vinland. It's said to be warm and fertile, a place where there would be no need for fighting—not at all like the frozen village in Iceland";
+   let age = 19;
     return (
       <div>
         <img src="/public/Vinnland.png" alt="ok" width = "40%" height = "40%"/>
@@ -12,8 +13,8 @@ const App = () => {
       <h2>Rating = {rating}</h2>
       <h3>Genre : {genre}</h3>
       <p>Summary : {summary} </p>
+      <button>{age >= 18 ? "Watch Now" : "Not Available"} </button>
       </div>
-
     )
   }
   return (
