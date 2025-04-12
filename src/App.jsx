c6d034486332545ee133f87ca1fe39a19d2ba8ab
FarazAyaz <ayazf313@gmail.com>
2025-04-12 17:11:38 +0500

    Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,8 @@
 import React from 'react'
+import AnimeSeries from './components/AnimeSeries';
+import { Footer } from './components/AnimeSeries'; 
 const App = () => {
-  const AnimeSeries = () => {
-    const anime = "Vinland Saga";
-    const rating = 9.5;
-    const genre = "Action, Adventure, Drama";
-    const summary = "Young Thorfinn grew up listening to the stories of old sailors that had traveled the ocean and reached the place of legend, Vinland. It's said to be warm and fertile, a place where there would be no need for fighting—not at all like the frozen village in Iceland";
-   let age = 19;
-    return (
-      <div>
-        <img src="/public/Vinnland.png" alt="ok" width = "40%" height = "40%"/>
-      <h1>Name : {anime}</h1>
-      <h2>Rating = {rating}</h2>
-      <h3>Genre : {genre}</h3>
-      <p>Summary : {summary} </p>
-      <button>{age >= 18 ? "Watch Now" : "Not Available"} </button>
-      </div>
-    )
-  }
+  <AnimeSeries/>
   return (
     <div>
       <AnimeSeries/>
@@ -24,6 +10,7 @@ const App = () => {
       <AnimeSeries/>
       <AnimeSeries/>
       <AnimeSeries/>
+      <Footer/>
 
     </div>
   )
