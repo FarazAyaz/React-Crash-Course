cafce0a30068fadd15379305eb4bdd5173caf61d
FarazAyaz <ayazf313@gmail.com>
2025-04-12 21:06:25 +0500

    looping in jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
 import React from 'react'
 import AnimeSeries from './components/AnimeSeries';
-import { Footer } from './components/AnimeSeries'; 
 const App = () => {
   <AnimeSeries/>
   return (
     <div>
       <AnimeSeries/>
-      <AnimeSeries/>
-      <AnimeSeries/>
-      <AnimeSeries/>
-      <AnimeSeries/>
-      <Footer/>
 
     </div>
   )
--- src/components/AnimeSeries.jsx
+++ src/components/AnimeSeries.jsx
@@ -1,23 +1,25 @@
+import SeriesData from "../api/seriesData.json";
+
 const AnimeSeries = () => {
-    const anime = "Vinland Saga";
-    const rating = 9.5;
-    const genre = "Action, Adventure, Drama";
-    const summary = "Young Thorfinn grew up listening to the stories of old sailors that had traveled the ocean and reached the place of legend, Vinland. It's said to be warm and fertile, a place where there would be no need for fighting—not at all like the frozen village in Iceland";
-   let age = 19;
-    return (
-      <div>
-        <img src="/public/Vinnland.png" alt="ok" width = "40%" height = "40%"/>
-      <h1>Name : {anime}</h1>
-      <h2>Rating = {rating}</h2>
-      <h3>Genre : {genre}</h3>
-      <p>Summary : {summary} </p>
-      <button>{age >= 18 ? "Watch Now" : "Not Available"} </button>
-      </div>
-    )
-}
-    export const Footer = () => {
-    return (
-        <p>Copyright @FARAZAYAZ</p>
-    )
-}
-    export default AnimeSeries;
+  return (
+    <ul>
+      {SeriesData.map((Series) => (
+        <li key={Series.id}>
+          <div>
+            <img src={Series.img_url} alt="ok" width="40%" height="40%" />
+            <h1>Name : {Series.name}</h1>
+            <h2>Rating = {Series.rating}</h2>
+            <h3>Genre : {Series.genre}</h3>
+            <p>Caste : {Series.cast}</p>
+            <p>Summary : {Series.description}</p>
+            <a href={Series.watch_url} target="_blank" rel="noreferrer">
+              <button>Watch Now</button>
+            </a>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default AnimeSeries;
