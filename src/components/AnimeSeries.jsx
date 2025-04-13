d5bc3c7869c3121e61af1447afd8c6c1d7e85a0a
FarazAyaz <ayazf313@gmail.com>
2025-04-13 19:57:50 +0500

    Destructuring Props

--- src/components/AnimeSeries.jsx
+++ src/components/AnimeSeries.jsx
@@ -1,22 +1,10 @@
 import SeriesData from "../api/seriesData.json";
-
+import SeriesCard from "./SeriesCard";
 const AnimeSeries = () => {
   return (
     <ul>
       {SeriesData.map((Series) => (
-        <li key={Series.id}>
-          <div>
-            <img src={Series.img_url} alt="ok" width="40%" height="40%" />
-            <h1>Name : {Series.name}</h1>
-            <h2>Rating = {Series.rating}</h2>
-            <h3>Genre : {Series.genre}</h3>
-            <p>Caste : {Series.cast}</p>
-            <p>Summary : {Series.description}</p>
-            <a href={Series.watch_url} target="_blank" rel="noreferrer">
-              <button>Watch Now</button>
-            </a>
-          </div>
-        </li>
+       <SeriesCard key= {Series.id} Data={Series}/>
       ))}
     </ul>
   );
---
+++ src/components/SeriesCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const SeriesCard = ({ Data }) => {
+  console.log(Data);
+  const {img_url, name, rating, genre, cast, description, watch_url } = Data;
+  return (
+    <li>
+        <img src={img_url} alt={name} width="40%" height="40%" />
+        <h1>Name: {name}</h1>
+        <h2>Rating: {rating}</h2>
+        <h3>Genre: {genre}</h3>
+        <p>Cast: {cast}</p>
+        <p>Summary: {description}</p>
+        <a href={watch_url} target="_blank" rel="noreferrer">
+          <button>Watch Now</button>
+        </a>
+    </li>
+  );
+};
+
+export default SeriesCard;
