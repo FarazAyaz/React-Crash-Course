45d6805187b6b5dbd5f3088571aa4121dcaf139c
FarazAyaz <ayazf313@gmail.com>
2025-04-19 23:42:01 +0500

    passing events as props

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import React from 'react'
 import AnimeSeries from './components/AnimeSeries';
 // import Profile from './components/Profile';
 import EventHandling from './components/EventHandling'
+import EventProps from './components/EventProps';
 const App = () => {
   // <AnimeSeries/>
-  <EventHandling/>
-
+  // <EventHandling/>
+  <EventProps/>
   return (
     <div>
       {/* <h1>Netflix Top Best Series</h1> */}
       {/* <AnimeSeries/> */}
       {/* <Profile/> */}
-      <EventHandling/>
+      {/* <EventHandling/> */}
+      <EventProps/>
      
 
     </div>
---
+++ src/components/EventProps.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+
+const EventProps = () => {
+    const HandleWelcomeUser = (user) =>{
+        alert(`My Name is ${user}`)
+    };
+const handleHover = () =>{
+    alert("Thanks Faraz For Hovering Me")
+};
+  return (
+    <>
+      <WelcomeUser onClick = {()=>HandleWelcomeUser("Faraz")}
+        onMouseEnter = {handleHover}
+        />
+    </>
+  )
+}
+const WelcomeUser = (props) => {
+    const {onClick, onMouseEnter} = props;
+    const HandleGreeting = ()=> {
+        console.log("HEllo Faraz");
+        }
+    return (
+        <>
+        <button onClick={onClick}>Click Me</button>
+        <button onMouseEnter={onMouseEnter}>Hover Me</button>
+        <button onClick={HandleGreeting}>Greeting</button>
+        
+        </>
+    )
+}
+export default EventProps
+// import React from 'react';
+
+// const WelcomeUser = (props) => {
+//   const Greeting = () => {
+//     console.log("Hello Faraz");
+//     props.onClick();
+//   };
+
+//   return (
+//     <>
+//       <button onClick={props.onClick}>Click Me</button>
+//       <button onMouseEnter={props.onMouseEnter}>Hover Me</button>
+//       <button onClick={Greeting}>Greeting</button>
+//     </>
+//   );
+// };
+
+// const EventProps = () => {
+//   const HandleWelcomeUser = (user) => {
+//     alert(`Welcome ${user}`);
+//   };
+
+//   const handleHover = () => {
+//     alert("Thanks Faraz For Hovering Me");
+//   };
+
+//   return (
+//     <>
+//       <WelcomeUser
+//         onClick={() => HandleWelcomeUser("Faraz")}
+//         onMouseEnter={handleHover}
+//       />
+//     </>
+//   );
+// };
+
+// export default EventProps;
