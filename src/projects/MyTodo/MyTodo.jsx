4d390c60b8d5243d413de2475ed9d79fe8749107
FarazAyaz <ayazf313@gmail.com>
2025-05-19 23:49:56 +0500

    edit

--- src/projects/MyTodo/MyTodo.jsx
+++ src/projects/MyTodo/MyTodo.jsx
@@ -1,17 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import { MdCheck, MdDeleteForever } from 'react-icons/md';
+import React, { useEffect, useState } from "react";
+import { MdCheck, MdDeleteForever, MdEdit } from "react-icons/md";
 
 const MyTodo = () => {
-  const todokey = "reactTodo"
+  const todokey = "reactTodo";
   const [inputValue, setInputValue] = useState("");
-  const [task, setTask] = useState(
-    () => {
-      const rawtodo = localStorage.getItem(todokey)
-      if (!rawtodo) return [];
-      return JSON.parse(rawtodo)
-    }
-  );
-  const [dateTime, setDateTime] =  useState("");
+  const [updateTodo, setUpdateTodo] = useState("");
+  const [isUpdating, setIsUpdating] = useState(null);
+  const [task, setTask] = useState(() => {
+    const rawtodo = localStorage.getItem(todokey);
+    if (!rawtodo) return [];
+    return JSON.parse(rawtodo);
+  });
+  const [dateTime, setDateTime] = useState("");
 
   const handleInputChange = (value) => {
     setInputValue(value);
@@ -23,7 +23,6 @@ const MyTodo = () => {
       return;
     }
 
- 
     // Check for duplicate tasks
     if (task.some((t) => t.content === inputValue)) {
       alert("Task already exists");
@@ -41,50 +40,62 @@ const MyTodo = () => {
     setTask((prevTask) => [...prevTask, todo]);
     setInputValue("");
   };
-// add todo in Local storage
-localStorage.setItem(todokey, JSON.stringify(task))
+  // add todo in Local storage
+  localStorage.setItem(todokey, JSON.stringify(task));
   // DATE AND TIME
-  useEffect(()=> {
-    const Interval = setInterval(()=> {
-    const date = new Date ();
-  const formattedDate = date.toLocaleDateString();
-  const formattedTime = date.toLocaleTimeString();
-  setDateTime(`${formattedDate} - ${formattedTime}`); 
-  },1000)
-    return () => clearInterval(Interval)
-},[])
-   const handleDeleteTodo = (value) => {
-    const updatedTasks = task.filter((todo)=> todo.content !== value);
-    setTask(updatedTasks)
-  }
+  useEffect(() => {
+    const Interval = setInterval(() => {
+      const date = new Date();
+      const formattedDate = date.toLocaleDateString();
+      const formattedTime = date.toLocaleTimeString();
+      setDateTime(`${formattedDate} - ${formattedTime}`);
+    }, 1000);
+    return () => clearInterval(Interval);
+  }, []);
+  const handleDeleteTodo = (value) => {
+    const updatedTasks = task.filter((todo) => todo.content !== value);
+    setTask(updatedTasks);
+  };
   const handleCheckTodo = (value) => {
     setTask((prev) => {
-      return prev.map((todo)=> {
-        if (todo.content  === value)
+      return prev.map((todo) => {
+        if (todo.content === value)
           return {
-           ...todo,
+            ...todo,
             state: todo?.state === "pending" ? "completed" : "pending",
-          }
+          };
         else return todo;
-      })
-    })
-  }
+      });
+    });
+  };
+  const handleUpdateTodo = (id) => {
+    setTask((pre) => {
+      return pre.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            content: updateTodo,
+          };
+        } else return todo;
+      });
+    });
+    setUpdateTodo(null);
+  };
 
- 
   return (
-    <div className='flex flex-col justify-center items-center h-screen bg-gray-200 w-screen'>
-      <h1 className='font-semibold text-3xl '>All Tasks</h1>
-      <h1 className='mt-5 font-bold text-3xl'>{dateTime}</h1>
-      <div className='flex w-[40%] mt-8 gap-8 items-center justify-center'>
-        <input 
-          type="search" 
-          placeholder='Enter Todo' 
+    <div className="flex flex-col justify-center items-center h-screen bg-gray-200 w-screen">
+      <h1 className="font-semibold text-3xl ">All Tasks</h1>
+      <h1 className="mt-5 font-bold text-3xl">{dateTime}</h1>
+      <div className="flex w-[40%] mt-8 gap-8 items-center justify-center">
+        <input
+          type="search"
+          placeholder="Enter Todo"
           value={inputValue}
           onChange={(event) => handleInputChange(event.target.value)}
-          className ='w-full px-4 py-2 rounded-md outline-none border border-gray-500 focus:border-yellow-500' 
+          className="w-full px-4 py-2 rounded-md outline-none border border-gray-500 focus:border-yellow-500"
         />
-        <button 
-          className='bg-yellow-500 text-white px-2 rounded-lg hover:bg-yellow-600 transition duration-300 ease-in-out cursor-pointer'
+        <button
+          className="bg-yellow-500 text-white px-2 rounded-lg hover:bg-yellow-600 transition duration-300 ease-in-out cursor-pointer"
           onClick={handleAddTodo}
         >
           Add Todo
@@ -92,36 +103,69 @@ localStorage.setItem(todokey, JSON.stringify(task))
       </div>
 
       {task.length === 0 ? (
-        <div className='flex justify-center items-center mt-10'> 
-          <h1 className='text-2xl font-semibold'>No Task Found</h1> 
+        <div className="flex justify-center items-center mt-10">
+          <h1 className="text-2xl font-semibold">No Task Found</h1>
         </div>
       ) : (
         <ul className="space-y-4  w-full max-w-[60%] mt-8">
           {task.map((todo) => (
-            <li key={todo.id} className={`bg-white ${
-                    todo?.state === "completed"
-                      ? "border-2 border-yellow-600 shadow-yellow-400"
-                      : ""
-                  } p-4 flex items-center justify-between flex-1 rounded-lg shadow-md`}
-                 > 
-              <span>{todo.content}</span> 
-              <div className='flex items-center gap-4'>
-                <button >
-                <span className='text-red-500 hover:text-red-700 transition duration-300 ease-in-out cursor-pointer'>
-                  <MdDeleteForever className='w-[30px] h-[30px]' onClick={()=> handleDeleteTodo(todo.content)}/> 
-                </span>
-              </button> 
-              <button>
-                <span className='text-green-500 hover:text-green-700 transition duration-300 ease-in-out cursor-pointer'>
-                  <MdCheck className='w-[30px] h-[30px]' onClick={() => handleCheckTodo(todo.content)}/> 
-                </span>
-              </button> 
+            <li
+              key={todo.id}
+              className={`bg-white ${
+                todo?.state === "completed"
+                  ? "border-2 border-yellow-600 shadow-yellow-400"
+                  : ""
+              } p-4 flex items-center justify-between flex-1 rounded-lg shadow-md`}
+            >
+              {isUpdating === todo.id ? (
+                <div className="flex flex-1 gap-4">
+                  <form onSubmit={() =>handleUpdateTodo(todo.id)} className="w-[80%]">
+                    <input
+                      type="search"
+                      placeholder="Update Todo"
+                      value={updateTodo}
+                      onChange={(event) => setUpdateTodo(event.target.value)}
+                      className="w-full px-4 py-2 rounded-md outline-none border border-gray-500 focus:border-yellow-500"
+                    />
+                  </form>
+                </div>
+              ) : (
+                <span>{todo.content}</span>
+              )}{" "}
+              <div className="flex items-center gap-4">
+                <button>
+                  <span className="text-blue-500 hover:text-blue-700 transition duration-300 ease-in-out cursor-pointer">
+                    <MdEdit
+                      className="w-[30px] h-[30px]"
+                      onClick={() => setIsUpdating(todo.id)}
+                    />
+                  </span>
+                </button>
+                <button>
+                  <span className="text-red-500 hover:text-red-700 transition duration-300 ease-in-out cursor-pointer">
+                    <MdDeleteForever
+                      className="w-[30px] h-[30px]"
+                      onClick={() => handleDeleteTodo(todo.content)}
+                    />
+                  </span>
+                </button>
+                <button>
+                  <span className="text-green-500 hover:text-green-700 transition duration-300 ease-in-out cursor-pointer">
+                    <MdCheck
+                      className="w-[30px] h-[30px]"
+                      onClick={() => handleCheckTodo(todo.content)}
+                    />
+                  </span>
+                </button>
               </div>
             </li>
           ))}
         </ul>
       )}
-      <button onClick={() => setTask([])} className='bg-red-500 text-white px-4 py-2 rounded-lg mt-5 hover:bg-red-600 transition duration-300 ease-in-out cursor-pointer'>
+      <button
+        onClick={() => setTask([])}
+        className="bg-red-500 text-white px-4 py-2 rounded-lg mt-5 hover:bg-red-600 transition duration-300 ease-in-out cursor-pointer"
+      >
         Clear All
       </button>
     </div>
