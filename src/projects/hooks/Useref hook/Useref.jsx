c593ce0a7fc7a7021130ae72ac4ac689cc7dcf62
FarazAyaz <ayazf313@gmail.com>
2025-06-24 23:53:50 +0500

    useref

---
+++ src/projects/hooks/Useref hook/Useref.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Useref = () => {
+  return (
+    <form>
+      <input type="text" />
+      <br />
+      <input type="text" />
+      <br />
+      <button onSubmit={handleonSubmit}>
+        submit
+      </button>
+    </form>
+  )
+}
+
+export default Useref
