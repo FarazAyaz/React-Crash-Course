fb9675cc8df9be7ab43fb2a4b9583250acbdb242
FarazAyaz <ayazf313@gmail.com>
2025-07-04 23:47:00 +0500

    commit

--- src/projects/hooks/UseReducer hook/UseReducer.jsx
+++ src/projects/hooks/UseReducer hook/UseReducer.jsx
@@ -9,9 +9,9 @@ const UseReducer = () => {
       //   return state - 1;
         switch (action.type) {
         case "Increment":
-        return state + 2;
+        return state + 1;
         case "Decrement":
-        return state - 2
+        return state - 1;
         case "Reset":
         return (0);
         default:
